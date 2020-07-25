518bdb0d26420e0ad4e2d512fe571a6732036f85
pcw <m13421653181@163.com>
2020-07-25 15:32:09 +0800

    tijiao

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
-# https://github.com/401227435/blog.git
-
 app=Flask(__name__)
 
 @app.route('/index')
 def index():
     return 'index'
+
 if __name__=='__main__':
     app.run()
